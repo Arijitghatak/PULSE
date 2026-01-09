f800fe163cee9b20ea018cd0f7a969fd40debacd
Arijitghatak <arijitgrowww@gmail.com>
2026-01-09 15:09:16 +0530

    Client part is started built

--- Client.java
+++ Client.java
@@ -1,4 +1,20 @@
+ import java.net.*;
+ 
  class Client {
+
+    Socket socket ;
+
+Public Client (){
+    try {
+        System.out.println("sending Request to server ");
+        socket = new Socket("i27.0.0.1",7777);
+
+
+    } catch (Exception e) {
+        printStackTrace();
+    }
+}
+
     public static void main(String[] args) {
         System.out.println("(client server is running)");
     }
